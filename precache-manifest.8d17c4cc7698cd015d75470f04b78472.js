99a215f5f2dea953b2374d529977bb470cca591f
William Mura <william57m@gmail.com>
2019-12-08 13:07:16 -0500

    Updates

---
+++ precache-manifest.8d17c4cc7698cd015d75470f04b78472.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db11b2e8f15c4b8bd7ef9a64e5cd2c75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebeb5fcfd2700c69067d",
+    "url": "/static/css/2.9bd960b0.chunk.css"
+  },
+  {
+    "revision": "dbff904f67be581226b4",
+    "url": "/static/css/main.fa4fe097.chunk.css"
+  },
+  {
+    "revision": "ebeb5fcfd2700c69067d",
+    "url": "/static/js/2.10ac6c0a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.10ac6c0a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbff904f67be581226b4",
+    "url": "/static/js/main.2584e39e.chunk.js"
+  },
+  {
+    "revision": "157bed256dd7dee7dd22",
+    "url": "/static/js/runtime-main.ce7736f8.js"
+  },
+  {
+    "revision": "5fbd70c5077087b45add01329ea35620",
+    "url": "/static/media/FuturaPT-Medium.5fbd70c5.woff2"
+  },
+  {
+    "revision": "995c64e152e8dc077222766889194951",
+    "url": "/static/media/FuturaPT-Medium.995c64e1.woff"
+  }
+]);
