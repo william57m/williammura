c18cb36660d785aac42bb827f2058be3c24b681f
William Mura <william57m@gmail.com>
2019-12-08 17:36:25 -0500

    Update landscapes photo

--- src/pages/Landscape.jsx
+++ src/pages/Landscape.jsx
@@ -3,13 +3,11 @@ import { Carousel } from '../components/Carousel';
 
 export function Landscape() {
   const photos = [
-    {src: 'images/landscapes/IMG_7837.jpg', width: 900},
-    {src: 'images/landscapes/IMG_8265.jpg', width: 900},
-    {src: 'images/landscapes/IMG_2195.jpg', width: 900}
+    {src: 'images/landscapes/IMG_7837.jpg', width: 900}
   ];
   return (
     <div className="content">
-      <Carousel photos={photos} />
+      <Carousel photos={photos} settings={{loop: false}} />
     </div>
   );
 }
