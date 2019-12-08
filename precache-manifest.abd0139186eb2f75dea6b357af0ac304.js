f059bf19f64d5df0a71941df6d210392f3d4eed1
William Mura <william57m@gmail.com>
2019-12-08 17:59:32 -0500

    Updates

---
+++ precache-manifest.abd0139186eb2f75dea6b357af0ac304.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ae014310cd65a523bb5e4193d3b6c6",
+    "url": "/williammura/index.html"
+  },
+  {
+    "revision": "ebeb5fcfd2700c69067d",
+    "url": "/williammura/static/css/2.9bd960b0.chunk.css"
+  },
+  {
+    "revision": "b6c9aae1089ec4329afe",
+    "url": "/williammura/static/css/main.f65f9ced.chunk.css"
+  },
+  {
+    "revision": "ebeb5fcfd2700c69067d",
+    "url": "/williammura/static/js/2.10ac6c0a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/williammura/static/js/2.10ac6c0a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b6c9aae1089ec4329afe",
+    "url": "/williammura/static/js/main.ae004cab.chunk.js"
+  },
+  {
+    "revision": "29f0569f7ffbf39cccfa",
+    "url": "/williammura/static/js/runtime-main.132a97d4.js"
+  }
+]);
