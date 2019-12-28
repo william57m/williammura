0ce242d077dbe1714fb9ad82c5eca234b71ed9a6
William Mura <william57m@gmail.com>
2019-12-28 09:03:47 -0500

    Add photos

--- src/pages/Landscape.jsx
+++ src/pages/Landscape.jsx
@@ -3,11 +3,14 @@ import { Carousel } from '../components/Carousel';
 
 export function Landscape() {
   const photos = [
+    {src: './images/landscapes/IMG_1711.jpg', width: 900},
+    {src: './images/landscapes/IMG_1822.jpg', width: 900},
+    {src: './images/landscapes/IMG_2195.jpg', width: 900},
     {src: './images/landscapes/IMG_7837.jpg', width: 900}
   ];
   return (
     <div className="content">
-      <Carousel photos={photos} settings={{loop: false}} />
+      <Carousel photos={photos} settings={{loop: true}} />
     </div>
   );
 }
